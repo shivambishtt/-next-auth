8b93c7c28192e1987a21424e9072af11de025920
Shivam Bisht <rajshivam2745@gmail.com>
2025-05-01 23:35:56 +0530

    authentication flow add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
+"use client"
 import React from 'react'
 
 function page() {
   return (
-    <div>
-      <h1>Authentication System</h1>
-      <nav>
-        <h2>Login</h2>
-        <h2>Signup</h2>
-      </nav>
+    <div className='bg-green-700 flex item-center justify-between flex-col'>
+      <h1 className='text-3xl font-bold'>Expenses App</h1>
+      <h3>Go to the dashboard to keep track of your expenses</h3>
     </div>
   )
 }
