956177007886cc2d9cf57aa7c7466ab610c7426c
Shivam Bisht <rajshivam2745@gmail.com>
2025-05-03 00:21:07 +0530

    user model add

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,1 +1,31 @@
-import mongoose from "mongoose"
+import mongoose, { Schema, Document } from "mongoose"
+
+export interface User extends Document {
+    username: string,
+    email: string,
+    password: string,
+    fullname: string,
+    isVerified: boolean,
+    isAdmin: boolean,
+    forgotPasswordToken: string,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: string,
+    verifyTokenExpriy: Date,
+}
+
+export const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: true,
+        isUnique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email address"],
+        isUnique: true
+    }
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel 
