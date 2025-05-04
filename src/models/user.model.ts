298d4e1a4665044c49fdb304e9292ea5b63cd1cb
Shivam Bisht <rajshivam2745@gmail.com>
2025-05-04 22:50:55 +0530

    model add for user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -17,13 +17,27 @@ export const UserSchema: Schema<User> = new Schema({
     username: {
         type: String,
         required: true,
-        isUnique: true
+        unique: true
     },
     email: {
         type: String,
         required: [true, "Please provide email address"],
-        isUnique: true
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter password first"],
+    },
+    fullname: {
+        type: String,
+    },
+    isVerified: {
+        type: Boolean,
+    },
+    isAdmin: {
+        type: Boolean
     }
+
 })
 
 const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
