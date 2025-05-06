d8f02cb35dc028de83a33fffe5ff0f588c186fb1
Shivam Bisht <rajshivam2745@gmail.com>
2025-05-07 01:17:43 +0530

    nodemailer config add

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,9 +1,22 @@
 import nodemailer from "nodemailer"
 
-export const sendEmail = async ({ email, emailType, userId }) => {
-
+export const sendEmail = async ({ email, emailType, userId }): Promise<void> => {
     try {
-
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: "maddison53@ethereal.email",
+                pass: "jn7jnAPss4f63QBp6D",
+            },
+        });
+        const mailOptions = {
+            from: 'shivam@shivamdev',
+            to: email,
+            subject: emailType === "VERIFY" ? "Verify Your Email" : "Reset Your Password",
+            html: "<b>Hello world?</b>",
+        }
     } catch (error) {
 
     }
