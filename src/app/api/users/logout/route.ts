20699edde17a3dfcd65ccf7c8aa96b8b7c97d32e
Shivam Bisht <rajshivam2745@gmail.com>
2025-05-18 23:52:34 +0530

    fixes and changes in logout route

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,24 +1,25 @@
 import connectDB from "@/db/dbConnect";
 import User from "@/models/user.model";
-import bcrypt from "bcrypt"
 import { NextRequest, NextResponse } from "next/server"
 
-export default async function POST(request: NextRequest) {
+export async function POST(request: NextRequest) {
     try {
-       const { username, password } = await request.json()
-    if (!username || !password) {
-        return NextResponse.json({ message: "Both fields are required" }, { status: 400 })
-    }
-    const user = await User.findOne({ username })
-    if (!user) {
-        return NextResponse.json({ message: "User with this username is not registered" }, { status: 400 })
-    }
-    const response = NextResponse.json({ message: "test" })
-    const accessToken = user.accessToken
-    const refreshToken = user.refreshToken
-    response.cookies.delete({ accessToken, refreshToken }) 
+        const { email, password } = await request.json()
+        if (!email || !password) {
+            return NextResponse.json({ message: "Both fields are required" }, { status: 400 })
+        }
+        const user = await User.findOne({ email })
+        if (!user) {
+            return NextResponse.json({ message: "User with this email is not registered" }, { status: 400 })
+        }
+        const response = NextResponse.json({ message: "User logged out successfully", sucess: true }, { status: 200 })
+        const accessToken = user.accessToken
+        const refreshToken = user.refreshToken
+        response.cookies.delete({ accessToken, refreshToken })
+        return response
     } catch (error) {
-        
+        return NextResponse.json({ message: error.message }, { status: 400 })
+
     }
-    
+
 }
