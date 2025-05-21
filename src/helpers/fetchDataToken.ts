0b381fc1188764dafdde8c86516e527e42faa5da
Shivam Bisht <rajshivam2745@gmail.com>
2025-05-22 00:00:00 +0530

    fixes add

--- src/helpers/fetchDataToken.ts
+++ src/helpers/fetchDataToken.ts
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken"
 export const getDataToken = (request: NextRequest) => {
     try {
         const accessToken = request.cookies.get("accessToken")?.value
-        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+        const decodedToken: any = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
         return decodedToken.id
     } catch (error) {
         throw new Error(error.message)
